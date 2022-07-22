c3791eb91205f0ce400298acaa6534540516f911
Deekshith Rathod <68671324+Deekshithrathod@users.noreply.github.com>
2022-07-22 10:17:07 +0530

    Best case scenario time O(n^2) to O(n)
    let arr = [1,2,3,4,5,6,7,8,9];
    Assume this is the array that you are sending to the function to sort, it's already sorted. In this case, without the flag, sorting would take O(n^2) but with the flag, it would just take O(n)

--- dsa-implementation/bubble-sort.js
+++ dsa-implementation/bubble-sort.js
@@ -6,12 +6,17 @@ function bubbleSort(array){
     arr = arr.concat(array);
     let n = arr.length;
     for(let i = 0; i < n; i++){
+        let isSorted = true;
         for(let j = 0; j < n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 let temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                isSorted = false;
             }
+        }
+        if(isSorted){
+            break;
         }
     }
     return arr;
